c5cd404c987778c0ff00bfb2514558b9a2a30fa6
Дарья Матвиевская <dollymo015@gmail.com>
2023-04-06 14:02:07 +0300

    Change csv writer.

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -11,22 +11,20 @@ class PepParsePipeline:
         self.results_dir = BASE_DIR / 'results'
         self.results_dir.mkdir(exist_ok=True)
         self.status_summary = defaultdict(int)
-        self.pep_total = 0
 
     def process_item(self, item, spider):
         self.status_summary[item['status']] += 1
-        self.pep_total += 1
         return item
 
     def close_spider(self, spider):
-        time_stamp = dt.now().strftime("%Y-%m-%d_%H-%M-%S")
+        pep_total = sum(self.status_summary.values())
+        time_stamp = dt.now().strftime('%Y-%m-%d_%H-%M-%S')
         filename = f'status_summary_{time_stamp}.csv'
         filepath = f'{self.results_dir}/{filename}'
 
         with open(filepath, mode='w', encoding='utf-8', newline='') as f:
-            fieldnames = ['Статус', 'Количество']
-            writer = csv.DictWriter(f, fieldnames=fieldnames)
-            writer.writeheader()
-            for status, amount in self.status_summary.items():
-                writer.writerow({'Статус': status, 'Количество': amount})
-            f.write(f'Total,{self.pep_total}\n')
+            headers = ('Статус', 'Количество')
+            writer = csv.writer(f)
+            writer.writerow(headers)
+            writer.writerows(sorted(self.status_summary.items()))
+            writer.writerow(('Total', pep_total))
